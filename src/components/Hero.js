ab10cf1067704c26f498498d6181603c732bd543
micariZ <qikadate@aliyun.com>
2021-09-28 00:57:47 -0400

    added hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "../style/hero.css";
+import SearchBar from "./common/SearchBar";
 
 function Hero() {
-  return <div className="container">Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1 className="hero-header">
+          The best free stock photos &#38; videos shared by talented creators.
+        </h1>
+        <SearchBar></SearchBar>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
